33fbb22feb3411e9e0f3b7f1e6ef4a6ff78e24ae
alexmagwe <alexmagwe@gmail.com>
2020-09-17 16:59:09 +0300

    added shell

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 web: gunicorn run:app
 upgrade: python manage.py db upgrade
-setup: python setup.py
+shell: python manage.py shell
--- setup.py
+++
@@ -1,9 +0,0 @@
-from app.models import AdminsList
-def setup():
-    u=AdminsList.query.filter_by(email=os.environ.get('SUPER')).first()
-    if not u:
-        a=AdminsList(email=os.environ.get('SUPER'))
-        res=a.add()
-    return res
-if __name__=='__main__':
-    setup()
