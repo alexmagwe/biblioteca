1a520085f6186a9eb79ce6168bb5cb62f3c14369
alexmagwe <alexmagwe@gmail.com>
2020-09-23 16:25:11 +0300

    moved db init before checking driver name

--- app/__init__.py
+++ app/__init__.py
@@ -33,11 +33,11 @@ def create_app(env='development'):
     mail.init_app(app)
     login_manager.init_app(app)
     admin.init_app(app)
+	db.init_app(app)
     if db.engine.url.drivername == 'sqlite':
         migrate.init_app(app, db, render_as_batch=True)
     else:
     	migrate.init_app(app,db)
-    db.init_app(app)
     bootstrap.init_app(app)
     from .api import api
     app.register_blueprint(api) 
