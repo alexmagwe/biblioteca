eb7306cda23c105e09377af63c93665ead3f37f0
alexmagwe <alexmagwe@gmail.com>
2020-10-23 01:19:51 +0300

    made admin menu items links

--- app/main/routes.py
+++ app/main/routes.py
@@ -7,6 +7,7 @@ from . import main
 @main.route('/')
 def home():
     if current_user.is_authenticated:
+        print('user is authenticated')
         return render_template('landing.html')
     else:
         return render_template('home.html')
@@ -20,7 +21,24 @@ def new_course():
 @main.route('/find_notes',methods=['GET'])
 def find_notes():
     return render_template('notes.html')
-    
+
+@main.route('/courses',methods=['GET'])
+def courses():
+    return render_template('data/courses.html')
+
+@main.route('/units',methods=['GET'])
+def units():
+    return render_template('data/units.html')
+
+@main.route('/notes',methods=['GET'])
+def notes():
+    return render_template('data/notes.html')
+
+@main.route('/users',methods=['GET'])
+def users():
+    return render_template('data/users.html')
+
+
 
 @main.route('/upload_notes',methods=['GET'])
 def upload_notes():
